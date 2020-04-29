cda41df175449a6929e76dd2a866b9d1934703fd
Fletcher <bassmattf@gmail.com>
2020-04-29 11:54:42 -0500

    Added deaths/cases specifier

--- src/Parser.py
+++ src/Parser.py
@@ -46,10 +46,38 @@ class Parser:
         self.downloadData()
         self.parsetojson()
 
-    def plotCounty(self, fips):
-        countydata = self.data[fips]['data']
+    def plotCounty(self, fips, d='d'):
+        try:
+            countydata = self.data[fips]['data']
+        except KeyError:
+            print(f"{fips} is not a valid FIPS code.")
+            return
         dates, cases, deaths = zip(*[(d['date'], d['cases'], d['deaths']) for d in countydata])
         dates = [dt.datetime.strptime(date, '%Y-%m-%d') for date in dates]
+        if d=='d':
+            plt.plot(dates, deaths)
+        elif d=='c':
+            plt.plot(dates, cases)
+        else:
+            print("Invalid Specifier")
+            return
+        plt.title(f"{'Cases' if d=='c' else 'Deaths' if d=='d' else 'INVALID'} in {self.data[fips]['county']} County, {self.data[fips]['state']}")
+        plt.show()
+
+
+    def compareCounties(self, fips1, fips2):
+        try:
+            c1, c2 = self.data[fips1], self.data[fips2]
+        except KeyError:
+            print(f"{fips} is not a valid FIPS code.")
+            return
+        dates1, cases1, deaths1 = zip(*[(d['date'], d['cases'], d['deaths']) for d in c1['data']])
+        dates2, cases2, deaths2 = zip(*[(d['date'], d['cases'], d['deaths']) for d in c2['data']])
+        dates1, dates2 = [dt.datetime.strptime(date, '%Y-%m-%d') for date in dates1], [dt.datetime.strptime(date, '%Y-%m-%d') for date in dates2]
+        print(dates1)
+        print(dates2)
+        dates = list(set(dates1+dates2))
+        plt.plot(dates,cases1,cases2)
         plt.title(f"Cases in {self.data[fips]['county']} County, {self.data[fips]['state']}")
-        plt.scatter(dates, cases)
+        
  
